579b2dff56547c7c08828b37d173a77e4125dace
Cristopher Soto Ventura <20213tn044@utez.edu.mx>
2024-10-04 17:18:28 -0600

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 module.exports = {
-    entry: './public/index.js',
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js',
-    },
-    plugins: [
-        new InjectManifest({
-            swSrc: './service-worker.js',
-            swDest: 'service-worker.js',
-        }),
-    ],
+  entry: './public/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new InjectManifest({
+      swSrc: './service-worker.js',
+      swDest: 'service-worker.js',
+    }),
+  ],
 };
