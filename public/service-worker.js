6fa37fd3135a6b64fa0667e60f67d97779b57704
CrisSoven <20213tn044@utez.edu.mx>
2024-11-08 12:23:12 -0600

    [MODIFY] service worker corregido

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,40 +1,59 @@
-const currentCache = 'current-cache-v1.0.1'
+const currentCache = "current-cache-v1.0.1";
 
 const files = [
-  'https://reqres.in/api/users',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js',
-  'https://unpkg.com/sweetalert/dist/sweetalert.min.js',
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/js/app.js',
-  '/songs.html',
-  '/images/album1.jpg',
+  "https://reqres.in/api/users",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js",
+  "https://unpkg.com/sweetalert/dist/sweetalert.min.js",
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/songs.html",
+  "/images/album1.jpg",
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(currentCache).then(cache => {
+    caches.open(currentCache).then((cache) => {
       return cache.addAll(files);
     })
   );
-  console.log(':D');
+  console.log("Service worker instalado y archivos cacheados");
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames.filter(cacheName => {
-        return cacheName !== currentCache
-      }).map(cacheName => caches.delete(cacheName))
-    ))
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames
+          .filter((cacheName) => {
+            return cacheName !== currentCache;
+          })
+          .map((cacheName) => caches.delete(cacheName))
+      )
+    )
+  );
+  console.log("Service worker activado y cachés antiguos eliminados");
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request)
+        .then((networkResponse) => {
+          return caches.open(currentCache).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => {});
+    })
   );
 });
-
-self.addEventListener('fetch', function (event) {
-  console.log('used to intercept requests so we can check for the file or data in the cache')
-})
 
 // const cleanCache = (cacheName, maxSize) => { //recibes un máximo de caches
 //   caches.open(cacheName)
